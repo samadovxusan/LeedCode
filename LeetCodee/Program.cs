601eba474dc4d11df93c03ddf724ac3ba5ba1cde
Husan <samadovxusan9013@gmail.com>
2025-04-04 18:05:19 +0500

    Add project files.

---
+++ LeetCodee/Program.cs
@@ -0,0 +1,12 @@
+﻿using LeetCodee;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = [2, 7, 11, 15,3,6,3,9,12,66,33];
+        int taget = 9;
+        var arr = TwoSumNums.Sum(nums, taget);
+
+    }
+}
---
+++ LeetCodee/TwoSumNums.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodee
+{
+    public static class TwoSumNums
+    {
+        public static int[] Sum(int[] nums, int target)
+        {
+            List<int> result = new();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        result.AddRange(i);
+                        result.AddRange(j);
+                    }
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
