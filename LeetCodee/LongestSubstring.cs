5d938fce1de7b7a21a55fafd70f016445a55e2d6
Husan <samadovxusan9013@gmail.com>
2025-04-17 16:39:40 +0500

    TwoSum

--- LeetCodee/LongestSubstring.cs
+++ LeetCodee/LongestSubstring.cs
@@ -1,5 +1,28 @@
 ﻿namespace LeetCodee;
 
-public class 3LongestSubstring {
-    
+public static class LongestSubstring{
+    public static int LengthOfLongestSubstring(string s)
+    {
+        int[] lastSeen = new int[128]; // ASCII uchun
+        for (int i = 0; i < 128; i++)
+            lastSeen[i] = -1;
+
+        int maxLength = 0;
+        int start = 0;
+
+        for (int end = 0; end < s.Length; end++)
+        {
+            char c = s[end];
+            if (lastSeen[c] >= start)
+            {
+                start = lastSeen[c] + 1;
+            }
+
+            lastSeen[c] = end;
+            maxLength = Math.Max(maxLength, end - start + 1);
+        }
+
+        return maxLength;
+    }
+tё
 }
--- LeetCodee/Program.cs
+++ LeetCodee/Program.cs
@@ -4,9 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[] nums = [2, 7, 11, 15,3,6,3,9,12,66,33];
-        int taget = 9;
-        var arr = TwoSumNums.Sum(nums, taget);
+        var sum = LongestSubstring.LengthOfLongestSubstring("abcabcbb");
+        Console.WriteLine(sum);
+
+
 
     }
 }
