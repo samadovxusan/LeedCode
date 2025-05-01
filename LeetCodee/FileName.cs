40e9215b6adcbaac5545d907cb95cf71a13e4420
Husan <samadovahurwida@gmail.com>
2025-05-01 22:00:17 +0500

    RemoveElement

---
+++ LeetCodee/FileName.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodee
+{
+    public static class RemoveElementArray
+    {
+            public static int RemoveElement(int[] nums, int val)
+            {
+                int result = 0;
+                foreach (var item in nums)
+                {
+                    if (item != val)
+                    {
+                        nums[result] = item;
+                        result++;
+                    }
+                }
+
+                return result;
+            }
+    }
+}
