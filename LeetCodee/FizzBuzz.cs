77c1cb28ed632f11dc7fd8c1996a97c94169001a
Husan <samadovxusan9013@gmail.com>
2025-06-03 10:54:33 +0500

    Add FizzBuzz

---
+++ LeetCodee/FizzBuzz.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCodee;
+
+public static class FizzBuzz
+{
+    public static void FizzaBuzz(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine($"FizzBuzz {i}");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine($"Fizz {i}");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine($"Buzz {i}");
+            }
+            else
+            {
+                Console.WriteLine($"Sonni ozi {i}");
+            }
+        }
+    }
+}
--- LeetCodee/Program.cs
+++ LeetCodee/Program.cs
@@ -1,18 +1,9 @@
-﻿using LeetCodee;
+﻿namespace LeetCodee;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var sum = SpiralMatrix.GenerateMatrix(3);
-        foreach (var index in sum)
-        {
-            foreach (var item in index)
-            {
-                Console.WriteLine($"{ item}  ");
-            }
-        }
-
-
+        FizzBuzz.FizzaBuzz(45);
     }
 }
