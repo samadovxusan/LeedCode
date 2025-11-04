c7f0352f299288aed3106b140638b538df5febaa
Husan <samadovxusan9013@gmail.com>
2025-11-04 16:15:57 +0500

    Add new Problem

---
+++ LeetCodee/Problems/FindXSums.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodee.Problems
+{
+    public static class FindXSums
+    {
+        public int[] FindXSum(int[] nums, int k, int x)
+        {
+             List<int> result = new();
+        int n = nums.Length;
+
+        for (int i = 0; i <= n - k; i++)
+        {
+            // 1️⃣ Subarraydagi elementlar
+            var freq = new Dictionary<int, int>();
+            for (int j = i; j < i + k; j++)
+            {
+                if (!freq.ContainsKey(nums[j]))
+                    freq[nums[j]] = 0;
+                freq[nums[j]]++;
+            }
+
+            // 2️⃣ Eng ko‘p uchragan x ta elementni topamiz
+            var topX = freq
+                .OrderByDescending(p => p.Value)  // Chastotaga ko‘ra
+                .ThenByDescending(p => p.Key)     // Agar chastota teng bo‘lsa — kattasini tanla
+                .Take(x)
+                .Select(p => p.Key)
+                .ToList();
+
+            // 3️⃣ Shu elementlarni subarraydan yig‘ib chiqamiz
+            int sum = 0;
+            for (int j = i; j < i + k; j++)
+            {
+                if (topX.Contains(nums[j]))
+                    sum += nums[j];
+            }
+
+            result.Add(sum);
+        }
+
+        return result.ToArray();
+        }
+    }
+}
---
+++ LeetCodee/Problems/MinCosts.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodee.Problems
+{
+    public static class MinCosts
+    {
+        public static int MinCost(string colors, int[] neededTime)
+        {
+            int totalTime = 0;
+            int n = colors.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (colors[i] == colors[i + 1])
+                {
+                    if (neededTime[i] < neededTime[i + 1])
+                    {
+                        totalTime += neededTime[i];
+                    }
+                    else
+                    {
+                        totalTime += neededTime[i + 1];
+                        neededTime[i + 1] = neededTime[i];
+                    }
+                }
+            }
+            return totalTime;
+
+        }
+    }
+}
